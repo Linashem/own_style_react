ddc7f68beb3fd1dfb298311238f5a1c99cab4d2d
Linashem <lina.shemeneva@yandex.ru>
2025-04-20 17:32:55 +0300

    change sizes add button and links

--- src/Components/card/Card.jsx
+++ src/Components/card/Card.jsx
@@ -3,11 +3,16 @@ import style from "./Card.module.scss";
 
 export const Card = ({ text, imgSrc, price }) => {
   return (
-    <div className={style.card} style={{ backgroundImage: `url(${imgSrc})` }}>
-      <div className={style.text}>
-        <h4>{text}</h4>
-        <p>{price}</p>
+    <a href='' className={style.card_link}>
+      <div className={style.card} style={{ backgroundImage: `url(${imgSrc})` }}>
+        <button className={style.card_button}>Подробнее</button>
+        <div className={style.text}>
+          <h4>
+            <a className={style.link}>{text}</a>
+          </h4>
+          <p>{price}</p>
+        </div>
       </div>
-    </div>
+    </a>
   );
 };
