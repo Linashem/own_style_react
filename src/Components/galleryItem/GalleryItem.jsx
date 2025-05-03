d98549c9cedbf4c1f6854a4ad3021366c1e20f10
Linashem <lina.shemeneva@yandex.ru>
2025-05-03 13:50:47 +0300

    text in cards in course page

--- src/Components/galleryItem/GalleryItem.jsx
+++ src/Components/galleryItem/GalleryItem.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 
 export const GalleryItem = ({data_lg_size, className, data_src}) => {
+  window.addEventListener('beforeunload', () => {
+    console.log('User clicked back button');
+  });
   return (
-    <a data-lg-size={data_lg_size} className={className} data-src={data_src} >
+    <a  data-lg-size={data_lg_size} className={className} data-src={data_src} >
       <img className='img-responsive' src={data_src} />
     </a>
   );
