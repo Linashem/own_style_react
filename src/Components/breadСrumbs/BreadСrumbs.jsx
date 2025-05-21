47ec80f1b74d2a64382c4cd2bc5abfcbcfebca48
Linashem <lina.shemeneva@yandex.ru>
2025-05-21 19:14:28 +0300

    fix size of details

---
+++ src/Components/breadСrumbs/BreadСrumbs.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import style from "./BreadСrumbs.module.scss";
+
+export const BreadСrumbs = () => {
+  return (
+    <div>breadСrumbs</div>
+  )
+}
--- src/Components/oneCourse/OneCourse.jsx
+++ src/Components/oneCourse/OneCourse.jsx
@@ -4,6 +4,7 @@ import style from "./OneCourse.module.scss";
 import { oneCourseActions } from "../../store/oneCourseReducer";
 import { coursesPageData } from "../../helper/constants";
 import { BtnWrite } from "../btnWrite/BtnWrite";
+import { BreadСrumbs } from "../breadСrumbs/BreadСrumbs";
 
 export const OneCourse = () => {
   const courseData = useSelector((state) => state.course);
@@ -21,83 +22,94 @@ export const OneCourse = () => {
   }, []);
 
   return (
-    <div className={style.one_course_block}>
-      <div className={style.one_course_top}>
-        <img
-          className={style.img}
-          src={courseData.imgSrc}
-          alt={courseData.text}
-        />
-        <div className={style.text}>
-          <div className={style.top}>
-            <h2 className={style.title}>{courseData.text}</h2>
-            <p className={style.desc}>{courseData.desc}</p>
-          </div>
-          <div className={style.bottom}>
-            <div className={style.clock}>
-              <img className={style.clock_img} src='/clock.jpg' alt='' />
-              <p className={style.clock_text}>{courseData.time}</p>
+    <>
+      <div className={style.one_course_block}>
+    <BreadСrumbs/>
+        <div className={style.one_course_top}>
+          <img
+            className={style.img}
+            src={courseData.imgSrc}
+            alt={courseData.text}
+          />
+          <div className={style.text}>
+            <div className={style.top}>
+              <h2 className={style.title}>{courseData.text}</h2>
+              <p className={style.desc}>{courseData.desc}</p>
             </div>
-            <p className={style.price}>{courseData.price}</p>
+            <div className={style.bottom}>
+              <div className={style.clock}>
+                <img className={style.clock_img} src='/clock.jpg' alt='' />
+                <p className={style.clock_text}>{courseData.time}</p>
+              </div>
+              <p className={style.price}>{courseData.price}</p>
+            </div>
+            <BtnWrite />
           </div>
-        <BtnWrite/>
+        </div>
+        <h2 className={style.ask_title}>Что нас ждет на курсе</h2>
+        <div className={style.one_course_bottom}>
+          {courseData.componentsTitle1 && (
+            <div className={style.div_block1}>
+              <ul
+                className={`${style.one_course_desc_title} ${style.one_course_desc_title1}`}
+              >
+                <div className={style.ask_div}>
+                  {courseData.componentsTitle1}
+                </div>
+                {courseData.componentsDesc1 &&
+                  courseData.componentsDesc1.map((el, i) => (
+                    <li key={i}>{el}</li>
+                  ))}
+              </ul>
+            </div>
+          )}
+          {courseData.componentsTitle2 && (
+            <div className={style.div_block2}>
+              <ul
+                className={`${style.one_course_desc_title} ${style.one_course_desc_title2}`}
+              >
+                <div className={style.ask_div}>
+                  {courseData.componentsTitle2}
+                </div>
+                {courseData.componentsDesc2 &&
+                  courseData.componentsDesc2.map((el, i) => (
+                    <li key={i}>{el}</li>
+                  ))}
+              </ul>{" "}
+            </div>
+          )}
+          {courseData.componentsTitle3 && (
+            <div className={style.div_block3}>
+              <ul
+                className={`${style.one_course_desc_title} ${style.one_course_desc_title3}`}
+              >
+                <div className={style.ask_div}>
+                  {courseData.componentsTitle3}
+                </div>
+                {courseData.componentsDesc3 &&
+                  courseData.componentsDesc3.map((el, i) => (
+                    <li key={i}>{el}</li>
+                  ))}
+              </ul>
+            </div>
+          )}
+          {courseData.componentsTitle4 && (
+            <div className={style.div_block4}>
+              <ul
+                className={`${style.one_course_desc_title} ${style.one_course_desc_title4}`}
+              >
+                <div className={style.ask_div}>
+                  {courseData.componentsTitle4}
+                </div>
+                {courseData.componentsDesc4 &&
+                  courseData.componentsDesc4.map((el, i) => (
+                    <li key={i}>{el}</li>
+                  ))}
+              </ul>
+            </div>
+          )}
         </div>
       </div>
-      <h2 className={style.ask_title}>Что нас ждет на курсе</h2>
-      <div className={style.one_course_bottom}>
-        {courseData.componentsTitle1 && (
-          <div className={style.div_block1}>
-            <ul
-              className={`${style.one_course_desc_title} ${style.one_course_desc_title1}`}
-            >
-              <div className={style.ask_div}>{courseData.componentsTitle1}</div>
-              {courseData.componentsDesc1 &&
-                courseData.componentsDesc1.map((el, i) => (
-                  <li key={i}>{el}</li>
-                ))}
-            </ul>
-          </div>
-        )}
-        {courseData.componentsTitle2 && (
-          <div className={style.div_block2}>
-            <ul
-              className={`${style.one_course_desc_title} ${style.one_course_desc_title2}`}
-            >
-              <div className={style.ask_div}>{courseData.componentsTitle2}</div>
-              {courseData.componentsDesc2 &&
-                courseData.componentsDesc2.map((el, i) => (
-                  <li key={i}>{el}</li>
-                ))}
-            </ul>{" "}
-          </div>
-        )}
-        {courseData.componentsTitle3 && (
-          <div className={style.div_block3}>
-            <ul
-              className={`${style.one_course_desc_title} ${style.one_course_desc_title3}`}
-            >
-              <div className={style.ask_div}>{courseData.componentsTitle3}</div>
-              {courseData.componentsDesc3 &&
-                courseData.componentsDesc3.map((el, i) => (
-                  <li key={i}>{el}</li>
-                ))}
-            </ul>
-          </div>
-        )}
-        {courseData.componentsTitle4 && (
-          <div className={style.div_block4}>
-            <ul
-              className={`${style.one_course_desc_title} ${style.one_course_desc_title4}`}
-            >
-              <div className={style.ask_div}>{courseData.componentsTitle4}</div>
-              {courseData.componentsDesc4 &&
-                courseData.componentsDesc4.map((el, i) => (
-                  <li key={i}>{el}</li>
-                ))}
-            </ul>
-          </div>
-        )}
-      </div>
-    </div>
+    </>
   );
 };
