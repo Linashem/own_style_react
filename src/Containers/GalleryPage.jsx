1f0b8cb78b3cb46d7124c5c912d045f5d269a1f1
Linashem <lina.shemeneva@yandex.ru>
2025-05-24 19:52:49 +0300

    gallery styles

--- src/Containers/GalleryPage.jsx
+++ src/Containers/GalleryPage.jsx
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Gallery } from "../Components/Gallery/Gallery";
 import { Header } from "../Components/header/Header";
 
 export const GalleryPage = () => {
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+    });
+  }, []);
   return (
     <>
       <Header />
